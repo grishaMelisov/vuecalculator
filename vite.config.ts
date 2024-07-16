6808d5044db662f716184f6f1482b8c3df77ccc8
Grigoriy Melisov <urecherrydream@gmail.com>
2024-07-15 23:30:53 -0300

    git hub pages deployment base add

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 
-
+// https://vitejs.dev/config/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/vuecalculator",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
